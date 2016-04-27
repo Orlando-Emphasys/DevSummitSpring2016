139248f76d53acc5e5dcb814be59324cc42bf571
Orlando Rodriguez <ORodriguez@emphasys-software.com>
2016-04-27 09:19:50 -0400

    Added a little styling

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,4 @@
 class CommentsController < ApplicationController
-	http_basic_authenticated_with name: "admin", password: "secret", only: :destroy
 
 	def create
 		@article = Article.find(params[:article_id])
